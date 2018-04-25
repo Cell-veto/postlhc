2cda4924a6083e5140d8cf159b889d193a5ba7b8
Sebastian Kapfer <sebastian.kapfer@fau.de>
2018-04-25 13:38:36 +0200

    add tagged LJ variant

--- lj.cpp
+++ lj.cpp
@@ -3,5 +3,7 @@
 
 static Register <ChainRunner <LennardJones, Monodisperse2D>>
     one ("lj/mono2d");
+static Register <ChainRunner <LennardJones, Tagged <Monodisperse2D>>>
+    one_a ("lj/tagged_mono2d");
 static Register <ChainRunner <LennardJones, Monodisperse3D>>
     two ("lj/mono3d");
