5140991d430f2dc76d8df658efeb12c1cbad399e
Sebastian Kapfer <sebastian.kapfer@fau.de>
2018-04-25 12:30:54 +0200

    BUGFIX: init-coords written only if not recovering

--- main.cpp
+++ main.cpp
@@ -212,8 +212,11 @@ int main (int, const char **argv)
     if (write_gofr)
         gofr = make_correlator ("density", stor, 0.01, 0.);
 
-    stor->periods ().savetxt (prefix + "periods");
-    stor->save_data (prefix + "init-coords.dat");
+    if (!recover)
+    {
+        stor->periods ().savetxt (prefix + "periods");
+        stor->save_data (prefix + "init-coords.dat");
+    }
 
     // init chainrunner
     std::cerr << "inter " << inter << '\n';
